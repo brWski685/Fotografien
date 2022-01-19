117e25d7e832a6f47d421400ad99608c38d2b015
brWski685 <eliastamm02@gmail.com>
2022-01-19 14:37:33 +0100

    Initial commit

---
+++ fotografien.js
@@ -0,0 +1,51 @@
+
+function findImg1() {
+    let imgBig = document.getElementById("imgBig");
+    let imgSmall = document.getElementById("img1")
+    imgBig.src = imgSmall.src;
+}
+function findImg2() {
+    let imgBig = document.getElementById("imgBig");
+    let imgSmall = document.getElementById("img2")
+    imgBig.src = imgSmall.src;
+}
+function findImg3() {
+    let imgBig = document.getElementById("imgBig");
+    let imgSmall = document.getElementById("img3")
+    imgBig.src = imgSmall.src;
+}
+function findImg4() {
+    let imgBig = document.getElementById("imgBig");
+    let imgSmall = document.getElementById("img4")
+    imgBig.src = imgSmall.src;
+}
+function findImg5() {
+    let imgBig = document.getElementById("imgBig");
+    let imgSmall = document.getElementById("img5")
+    imgBig.src = imgSmall.src;
+}
+function findImg6() {
+    let imgBig = document.getElementById("imgBig");
+    let imgSmall = document.getElementById("img6")
+    imgBig.src = imgSmall.src;
+}
+function findImg7() {
+    let imgBig = document.getElementById("imgBig");
+    let imgSmall = document.getElementById("img7")
+    imgBig.src = imgSmall.src;
+}
+function findImg8() {
+    let imgBig = document.getElementById("imgBig");
+    let imgSmall = document.getElementById("img8")
+    imgBig.src = imgSmall.src;
+}
+function findImg9() {
+    let imgBig = document.getElementById("imgBig");
+    let imgSmall = document.getElementById("img9")
+    imgBig.src = imgSmall.src;
+}
+function findImg10() {
+    let imgBig = document.getElementById("imgBig");
+    let imgSmall = document.getElementById("img10")
+    imgBig.src = imgSmall.src;
+}
